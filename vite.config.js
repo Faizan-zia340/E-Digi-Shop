3829ba7c48345a8da09d5135bdfca8ae30aacbba
Faizan-zia340 <ziafaizan340@gmail.com>
2025-05-09 23:17:45 +0500

    di

--- vite.config.js
+++ vite.config.js
@@ -15,6 +15,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    chunkSizeWarningLimit: 1000, // Adjust the size limit as per your needs
+    chunkSizeWarningLimit:2000, // Adjust the size limit as per your needs
   },
 })
