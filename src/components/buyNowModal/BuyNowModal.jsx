659a2ff6e5b05a2d939d8fd826d69c4fc277f8e1
Faizan-zia340 <ziafaizan340@gmail.com>
2025-06-14 14:23:01 +0500

    order display on user dashboard

--- src/components/buyNowModal/BuyNowModal.jsx
+++ src/components/buyNowModal/BuyNowModal.jsx
@@ -1,69 +1,107 @@
+/* eslint-disable react/prop-types */
 import {
-  Button,
-  Dialog,
-  DialogBody,
+    Button,
+    Dialog,
+    DialogBody,
 } from "@material-tailwind/react";
 import { useState } from "react";
 
-const BuyNowModal = () => {
-  const [open, setOpen] = useState(false);
+const BuyNowModal = ({ addressInfo, setAddressInfo, buyNowFunction }) => {
+    const [open, setOpen] = useState(false);
 
-  const handleOpen = () => setOpen(!open);
-
-  return (
-    <>
-      <Button
-        type="button"
-        onClick={handleOpen}
-        className="w-full px-4 py-3 text-center text-gray-100 bg-purple-600 border border-transparent hover:border-purple-500 hover:text-purple-700 hover:bg-purple-100 rounded-xl"
-      >
-        Buy now
-      </Button>
-
-      <Dialog
-        open={open}
-        handler={handleOpen}
-        className="backdrop-blur-md bg-transparent flex items-center justify-center"
-      >
-        <DialogBody className="bg-purple-50 max-w-lg w-full mx-auto rounded-xl shadow-lg p-6">
-          <form className="space-y-4">
-            <input
-              type="text"
-              name="name"
-              placeholder="Enter your name"
-              className="bg-purple-50 border border-purple-200 px-3 py-2 w-full rounded-md outline-none text-purple-600 placeholder-purple-300"
-            />
-            <input
-              type="text"
-              name="address"
-              placeholder="Enter your address"
-              className="bg-purple-50 border border-purple-200 px-3 py-2 w-full rounded-md outline-none text-purple-600 placeholder-purple-300"
-            />
-            <input
-              type="number"
-              name="pincode"
-              placeholder="Enter your pincode"
-              className="bg-purple-50 border border-purple-200 px-3 py-2 w-full rounded-md outline-none text-purple-600 placeholder-purple-300"
-            />
-            <input
-              type="text"
-              name="mobileNumber"
-              placeholder="Enter your mobile number"
-              className="bg-purple-50 border border-purple-200 px-3 py-2 w-full rounded-md outline-none text-purple-600 placeholder-purple-300"
-            />
-
+    const handleOpen = () => setOpen(!open);
+    return (
+        <>
             <Button
-              type="submit"
-              onClick={handleOpen}
-              className="w-full px-4 py-3 text-center text-white bg-violet-500 hover:bg-purple-600 rounded-lg"
+                type="button"
+                onClick={handleOpen}
+                className="w-full px-4 py-3 text-center text-gray-100 bg-purple-600 border border-transparent dark:border-gray-700 hover:border-purple-500 hover:text-purple-700 hover:bg-purple-100 rounded-xl"
             >
-              Buy now
+                Buy now
             </Button>
-          </form>
-        </DialogBody>
-      </Dialog>
-    </>
-  );
-};
+            <Dialog open={open} handler={handleOpen} className=" bg-purple-50">
+                <DialogBody className="">
+                    <div className="mb-3">
+                        <input
+                            type="text"
+                            name="name"
+                            value={addressInfo.name}
+                            onChange={(e) => {
+                                setAddressInfo({
+                                    ...addressInfo,
+                                    name: e.target.value
+                                })
+                            }}
+                            placeholder='Enter your name'
+                            className='bg-purple-50 border border-purple-200 px-2 py-2 w-full rounded-md outline-none text-purple-600 placeholder-purple-300'
+                        />
+                    </div>
+                    <div className="mb-3">
+                        <input
+                            type="text"
+                            name="address"
+                            value={addressInfo.address}
+                            onChange={(e) => {
+                                setAddressInfo({
+                                    ...addressInfo,
+                                    address: e.target.value
+                                })
+                            }}
+                            placeholder='Enter your address'
+                            className='bg-purple-50 border border-purple-200 px-2 py-2 w-full rounded-md outline-none text-purple-600 placeholder-purple-300'
+                        />
+                    </div>
+
+                    <div className="mb-3">
+                        <input
+                            type="number"
+                            name="pincode"
+                            value={addressInfo.pincode}
+                            onChange={(e) => {
+                                setAddressInfo({
+                                    ...addressInfo,
+                                    pincode: e.target.value
+                                })
+                            }}
+                            placeholder='Enter your pincode'
+                            className='bg-purple-50 border border-purple-200 px-2 py-2 w-full rounded-md outline-none text-purple-600 text-purple-600 placeholder-purple-300'
+                        />
+                    </div>
+
+                    <div className="mb-3">
+                        <input
+                            type="text"
+                            name="mobileNumber"
+                            value={addressInfo.mobileNumber}
+                            onChange={(e) => {
+                                setAddressInfo({
+                                    ...addressInfo,
+                                    mobileNumber: e.target.value
+                                })
+                            }}
+                            placeholder='Enter your mobileNumber'
+                            className='bg-purple-50 border border-purple-200 px-2 py-2 w-full rounded-md outline-none text-purple-600 placeholder-purple-300'
+                        />
+                    </div>
+
+                    <div className="">
+                        <Button
+
+                            type="button"
+                            onClick={() => {
+                                handleOpen();
+                                buyNowFunction();
+                            }}
+                            className="w-full px-4 py-3 text-center bg-violet-300 hover:bg-violet-500 border-transparent dark:border-gray-700 rounded-lg"
+                        >
+                            Buy now
+                        </Button>
+                    </div>
+
+                </DialogBody>
+            </Dialog>
+        </>
+    );
+}
 
 export default BuyNowModal;
